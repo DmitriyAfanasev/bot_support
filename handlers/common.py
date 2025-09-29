724d1c9e2c549e78cbe8d31ab8d1325682047cd1
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-09-29 20:15:06 +0400

    hot fix

--- handlers/common.py
+++ handlers/common.py
@@ -88,9 +88,8 @@ async def send_support_contact(obj: Message | CallbackQuery) -> None:
 @router.message(CommandStart())
 async def handle_start(message: Message) -> None:
     """Обработчик команды /start: приветствие с меню."""
-    await message.answer_photo(
-        photo=FSInputFile("images/start.jpg"),
-        caption="👋 Привет! Это бот-помощник.\n\nВы можете открыть инструкции через кнопки ниже:",
+    await message.answer(
+       text="👋 Привет! Это бот-помощник.\n\nВы можете открыть инструкции через кнопки ниже:",
         reply_markup=start_kb(),
         parse_mode="HTML",
     )
